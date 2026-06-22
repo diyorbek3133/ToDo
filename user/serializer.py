6c399b6edb59a73d0bd70b48a2ee03ad7de0b68b
Diyorbek <chilmuhammedovdiyor11@gmail.com>
2026-06-22 17:36:03 +0500

    test

--- user/serializer.py
+++ user/serializer.py
@@ -26,3 +26,5 @@ class LoginUserSerializer(serializers.Serializer):
             raise serializers.ValidationError("user or password error")
         data["user"] = user
         return data
+    
+# test
